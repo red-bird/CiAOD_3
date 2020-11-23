b1b759e9454e4b58671c6c9cab39586d4cde5cfb
redbird <redbird8941@gmail.com>
2020-11-23 17:15:58 +0300

    Translate from rus to en

--- CiAOD_3/HashMap.h
+++ CiAOD_3/HashMap.h
@@ -1,161 +1,160 @@
-using namespace std;
-//template for generic type 
-template<typename K, typename V, typename T>
+	using namespace std;
 
-//Элемент-узел Хешмапы
-class HashNode
-{
-public:
-	V value;//цена
-	K key;//ключ
-	T name;//название
-
-	HashNode(K key)
+	template<typename K, typename V, typename T>
+	class HashNode
 	{
-		this->key = key;
-	}
+	public:
+		V value;
+		K key;
+		T name;
+		int id;
 
-	HashNode(K key, V value, T name)
+		HashNode(K key)
+		{
+			this->key = key;
+		}
+
+		HashNode(K key, V value, T name)
+		{
+			this->value = value;
+			this->key = key;
+			this->name = name;
+		}
+	};
+
+	template<typename K, typename V, typename T>
+	class HashMap
 	{
-		this->value = value;
-		this->key = key;
-		this->name = name;
-	}
-};
+		//Array of Node of pointers
+		HashNode<K, V, T> **arr;
+		int capacity;
+		//counter of elements
+		int size;
+		//Mock-node
+		HashNode<K, V, T> *mock;
 
-template<typename K, typename V, typename T>
-class HashMap
-{
-	//Массив указателей на узлы
-	HashNode<K, V, T> **arr;
-	int capacity;
-	//счетчик элементов
-	int size;
-	//фиктивный-узел
-	HashNode<K, V, T> *mock;
+	public:
+		HashMap(int capacity)
+		{
+			this->capacity = capacity;
+			size = 0;
+			arr = new HashNode<K, V, T>*[capacity];
 
-public:
-	HashMap(int capacity)
-	{
-		this->capacity = capacity;
-		size = 0;
-		arr = new HashNode<K, V, T>*[capacity];
+			//Initialize array of nullpointer
+			for (int i = 0; i < capacity; i++)
+				arr[i] = NULL;
 
-		//Инициализируем массив нуль-указателей
-		for (int i = 0; i < capacity; i++)
-			arr[i] = NULL;
+			mock = new HashNode<K, V, T>(-1);
 
-		mock = new HashNode<K, V, T>(-1);
-
-	}
+		}
 	
-	//Хеш-функций
-	int hashCode(K key)
-	{
-		return key % capacity;
-	}
-
-	//Функция вставки
-	bool insertNode(K key, V value, T name)
-	{
-		if (isFull()) { return false; };
-
-		HashNode<K, V, T> *temp = new HashNode<K, V, T>(key, value, name);
-
-		// Получение индекса
-		int hashIndex = hashCode(key);
-
-		//Ищем пустое место 1усл - если пусто, 2 - если уже занято, но заменяем, 3 - если фиктивный
-		while (arr[hashIndex] != NULL && arr[hashIndex]->key != key
-			&& arr[hashIndex]->key != -1)
+		//hash-function
+		int hashCode(K key)
 		{
-			hashIndex++;
-			hashIndex %= capacity;
+			return key % capacity;
 		}
 
-		//Если не заменяется, то увеличиваем счетчик
-		if (arr[hashIndex] == NULL || arr[hashIndex]->key == -1)
-			size++;
-		else
-			delete arr[hashIndex];
-		arr[hashIndex] = temp;
-		return true;
-	}
-
-	//Функция удаления узла
-	bool deleteNode(int key)
-	{
-		//Если и так пустая, то выходим
-		if (isEmpty()) { return false; };
-
-		// Получаем индекс
-		int hashIndex = hashCode(key);
-
-		//Ищем узел с заданным ключом
-		while (arr[hashIndex] != NULL)
+		//insert function
+		bool insertNode(K key, V value, T name)
 		{
-			//Если нашли
-			if (arr[hashIndex]->key == key)
+			if (isFull()) { return false; };
+
+			HashNode<K, V, T> *temp = new HashNode<K, V, T>(key, value, name);
+
+			// get index
+			int hashIndex = hashCode(key);
+			//Search empty space 1if - if empty, 2if - if already taken, 3if - if is mock
+			while (arr[hashIndex] != NULL && arr[hashIndex]->key != key
+				&& arr[hashIndex]->key != -1)
 			{
-				//Вместо найденного узла вставляем фиктивный-узел
-				arr[hashIndex] = mock;
-
-				// Ну и уменьшаем счетчик элементов
-				--size;
-				return true;
+				hashIndex++;
+				hashIndex %= capacity;
 			}
-			hashIndex++;
-			hashIndex %= capacity;
 
+			//if not replaced, then increase counter
+			if (arr[hashIndex] == NULL || arr[hashIndex]->key == -1)
+				size++;
+			else
+				delete arr[hashIndex];
+			arr[hashIndex] = temp;
+			temp->id = hashIndex;
+			return true;
 		}
 
-		//Если не нашли то выходим из функции
-		return false;
-	}
-
-	//Функцию поиска узла
-	HashNode<K, V, T>* getNode(int key)
-	{
-		//Получаем индекс, а также создаем защиту от бесконечного цикла
-		int hashIndex = hashCode(key);
-		int origIndex = hashIndex-1;
-
-		//Поиск нужного узла
-		while (arr[hashIndex] != NULL && hashIndex != origIndex)
+		//Node delete function
+		bool deleteNode(int key)
 		{
-			//Если нашли то возращает указатель на узел
-			if (arr[hashIndex]->key == key)
+			//if empty, then exit
+			if (isEmpty()) { return false; };
+
+			// get index
+			int hashIndex = hashCode(key);
+
+			//Search node with key
+			while (arr[hashIndex] != NULL)
+			{
+				//If found
+				if (arr[hashIndex]->key == key)
+				{
+					//Instead searched Node insert mock-node
+					arr[hashIndex] = mock;
+
+					// decrease counter of element
+					--size;
+					return true;
+				}
+				hashIndex++;
+				hashIndex %= capacity;
+
+			}
+
+			//If not found then exit the function
+			return false;
+		}
+
+		//Node search function
+		HashNode<K, V, T>* getNode(int key)
+		{
+			//Get index and create defense of infinity cycle
+			int hashIndex = hashCode(key);
+			int origIndex = hashIndex-1;
+
+			//Find desired node
+			while (arr[hashIndex] != NULL && hashIndex != origIndex)
+			{
+				// If found, it returns a pointer to the node
+				if (arr[hashIndex]->key == key)
+					return arr[hashIndex];
+				hashIndex++;
+				hashIndex %= capacity;
+			}
+			if (arr[hashIndex] != NULL && arr[hashIndex]->key == key)
 				return arr[hashIndex];
-			hashIndex++;
-			hashIndex %= capacity;
+
+			//If not found, return null pointer
+			return NULL;
 		}
-		if (arr[hashIndex] != NULL && arr[hashIndex]->key == key)
-			return arr[hashIndex];
 
-		//Если не нашли то возращаем нуль.указатель
-		return NULL;
-	}
+		int mapSize(){
+			return size;
+		}
 
-	int mapSize(){
-		return size;
-	}
+		bool isEmpty(){
+			return size == 0;
+		}
 
-	bool isEmpty(){
-		return size == 0;
-	}
+		bool isFull() {
+			return size == capacity;
+		}
 
-	bool isFull() {
-		return size == capacity;
-	}
-
-	//Вывод всех узлов
-	void displayMap()
-	{
-		for (int i = 0; i < capacity; i++)
+		//Display all nodes
+		void displayMap()
 		{
-			if (arr[i] != NULL && arr[i]->key != -1)
-				cout << "key = " << arr[i]->key
-				<< " price = " << arr[i]->value << " name = " << arr[i]->name << endl;
+			for (int i = 0; i < capacity; i++)
+			{
+				if (arr[i] != NULL && arr[i]->key != -1)
+					cout << "key = " << arr[i]->key << " ID = " << arr[i]->id
+					<< " price = " << arr[i]->value << " name = " << arr[i]->name << endl;
+			}
 		}
-	}
-};
+	};
--- CiAOD_3/Source.cpp
+++ CiAOD_3/Source.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 
 void showMenu() {
-	cout << "===========================Меню выбора===========================\n"
-		<< "1.  Вставка элемента\n"
-		<< "2.  Удаление элемента\n"
-		<< "3.  Поиск и считывание элемента\n"
-		<< "4.  Вывод хешмапы\n"
-		<< "5.  Завершить программу\n"
-		<< "Введите команду: ";
+	cout << "===========================Selection menu===========================\n"
+		<< "1.  Enter element\n"
+		<< "2.  Delete element\n"
+		<< "3.  Search and read element\n"
+		<< "4.  Display hashmap\n"
+		<< "5.  Stop program\n"
+		<< "Enter code: ";
 }
 
 int main()
@@ -26,102 +26,100 @@ int main()
 
 	};
 
-	setlocale(LC_ALL, "ru");
-	cout << "Хеширование - прямой доступ к данным\n18вариант прямая адресация(линейное пробирование)\nТовар: Код - шестиразрядное число, название цена\n";
+	//setlocale(LC_ALL, "ru");
+	cout << "Hashing - direct access to data\n18 Variant direct addressing (linear probing)\nProduct: Code - six-digit number, name, price\n";
 	int n;
-	cout << "Введите количество элементов хешмапы: ";
+	cout << "Enter amount of hashmap: ";
 	try
 	{
 		cin >> n;
 		if (n < 1)
 		{
-			cout << "Недопустимое количество элементов";
+			cout << "Wrong amount of elements";
 			return -1;
 		}
 	}
 	catch (const std::exception&)
 	{
-		cout << "Что-то пошло не так";
+		cout << "Something gone wrong.. stop programm";
 		return -1;
 	}
 
 	HashMap<int, double, string> *h = new HashMap<int, double, string>(n);
 	showMenu();
 	int key;
+	n = 0;
 	double price;	
 	string name;
+	HashNode<int, double, string>* tmp;
 	while (n != Stop)
 	{
 		cin >> n;
 		switch (n)
 		{
 		case(Insert):
-			cout << "Введите ключ/артикул 6-разрядный: ";
+			cout << "Enter key/article sit-digit: ";
 			cin >> key;
 			if ((key / 100000 > 9) | (key / 100000 < 1))
 			{
-				cout << "Артикул не 6-разрядное число\n";
+				cout << "Article is not 6-digit number\n";
 				return -1;
 			}
-			cout << "Введите название: ";
+			cout << "Enter name: ";
 			cin >> name;
-			cout << "Введите цену: ";
+			cout << "Enter price: ";
 			cin >> price;
 			if (h->insertNode(key, price, name))
-				cout << "Успешно!" << endl;
+				cout << "Success!" << endl;
 			else
-				cout << "Ошибка!" << endl;
+				cout << "Error!" << endl;
 			showMenu();
 			break;
 
 		case(Delete):
-			cout << "Введите ключ/артикул 6-разрядный: ";
+			cout << "Enter key/article sit-digit: ";
 			cin >> key;
 			if ((key / 100000 > 9) | (key / 100000 < 1))
 			{
-				cout << "Артикул не 6-разрядное число\n";
+				cout << "Article is not 6-digit number\n";
 				return -1;
 			}
 			if (h->deleteNode(key))
-				cout << "Успешно!" << endl;
+				cout << "Success!" << endl;
 			else
-				cout << "Ошибка!" << endl;
+				cout << "Error!" << endl;
 			showMenu();
 			break;
 
 		case(Search):
-			cout << "Введите ключ/артикул 6-разрядный: ";
+			cout << "Enter key/article sit-digit: ";
 			cin >> key;
 			if ((key / 100000 > 9) | (key / 100000 < 1))
 			{
-				cout << "Артикул не 6-разрядное число\n";
+				cout << "Article is not 6-digit number\n";
 				return -1;
 			}
-			try
-			{
-				cout << "Название = " << h->getNode(key)->name << "\nЦена = " << h->getNode(key)->value << endl;
-				cout << "Успешно!" << endl;
-			}
-			catch (const std::exception&)
-			{
-				cout << "Ошибка!" << endl;
-			}
+			tmp = h->getNode(key);
+			if (tmp != NULL)
+				cout << "key = " << tmp->key << " id = " << tmp->id << " name = " << tmp->name << " price = " << tmp->value << endl;
+			else
+				cout << "Error!" << endl;
 			showMenu();
 			break;
 
 		case(Display):
-			cout << "=============Все элементы=============" << endl;
+			cout << "=============All elements=============" << endl;
 			h->displayMap();
 			cout << endl;
 			showMenu();
 			break;
 
 		case(Stop):
-			cout << "Выход из программы" << endl;
+			cout << "Exit from program" << endl;
 			return 0;
 
 		default:
-			cout << "Что-то пошло не так..";
+			cout << "Something gone wrong...";
 			return -1;
 		}
 	}
